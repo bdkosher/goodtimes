9056a6a83ca389972b40a09f994ee20eab07a420
Joe Wolf <joewolf@gmail.com>
2017-01-15 23:09:02 -0500

    more Date/Calendar work

--- src/main/groovy/bdkosher/goodtimes/DateCalendarExtension.groovy
+++ src/main/groovy/bdkosher/goodtimes/DateCalendarExtension.groovy
@@ -3,19 +3,64 @@ package bdkosher.goodtimes
 import java.time.*
 
 /**
- * Adds methods to Date and Calendar instances for converting values into Java 8 Date/Time equivalents.
+ * Extension methods for converting or inspecting java.util.Date and java.util.Calendar instances as Java 8 Date/Time types.
  */
 class DateCalendarExtension {
 
     private static final DEFAULT_ZONE_ID = ZoneId.systemDefault()
 
+    /**
+     * Converts the given Date to a Calendar instance.
+     */
+    static Calendar toCalendar(final Date date) {
+        def cal = Calendar.instance
+        cal.time = date
+        cal
+    }
+
+    /**
+     * Returns the TimeZone of the given Date as a java.time.ZoneOffset.
+     */
+    static ZoneOffset getZoneOffset(final Date date) {
+        getZoneOffset(toCalendar(date))
+    }
+
+    /**
+     * Returns the TimeZone of the given Calendar as a java.time.ZoneOffset.
+     */
+    static ZoneOffset getZoneOffset(final Calendar cal) {
+        int offsetMs = cal.get(Calendar.ZONE_OFFSET) 
+        offsetMs += cal.get(Calendar.DST_OFFSET)
+        ZoneOffset.ofTotalSeconds(1000 * offsetMs)
+    }
+
+    /**
+     * Returns the TimeZone of the given Date as a java.time.ZoneId.
+     */
+    static ZoneId getZoneId(final Date date) {
+        getZoneId(toCalendar(date))
+    }
+
+    /**
+     * Returns the TimeZone of the given Calendar as a java.time.ZoneId.
+     */
+    static ZoneId getZoneId(final Calendar cal) {
+        cal.timeZone.toZoneId()
+    }
+
+    /**
+     * Converts the Date to a LocalDate, using the Date's own TimeZone to adjust the LocalDate accordingly.
+     * If the Date is of a different TimeZone than the system TimeZone, then the year/month/day of
+     * the generated LocalDate may differ from the year/month/day of the Date, depending on the difference
+     * between the TimeZone offsets and the time value of the Date instance.
+     */
     static toLocalDate(final Date self, ZoneId zoneId = DEFAULT_ZONE_ID) {
         toZonedDateTime(self, zoneId).toLocalDate()
     }
 
     static toLocalDate(final Date self, ZoneOffset offset) {
         toOffsetDateTime(self, offset).toLocalDate()
-    }    
+    }
 
     static toLocalTime(final Date self, ZoneId zoneId = DEFAULT_ZONE_ID) {
         toZonedDateTime(self, zoneId).toLocalTime()
@@ -34,16 +79,13 @@ class DateCalendarExtension {
     }    
     
     static toZonedDateTime(final Date self, ZoneId zoneId = DEFAULT_ZONE_ID) {
-        toInstant(self).atZone(zoneId)
+        self.toInstant().atZone(zoneId)
     }
 
     static toOffsetDateTime(final Date self, ZoneOffset offset) {
-        toInstant(self).atOffset(offset)
+        self.toInstant().atOffset(offset)
     }
 
-    static toInstant(final Date self) {
-        Instant.ofEpochMilli(self.time)
-    }
 
     static toLocalDate(final Calendar self, ZoneId zoneId = DEFAULT_ZONE_ID) {
         toLocalDate(self.time, zoneId)
@@ -78,6 +120,6 @@ class DateCalendarExtension {
     }    
 
     static toInstant(final Calendar self) {
-        toInstant(self.time)
+        self.time.toInstant()
     }
 }
--- src/test/groovy/bdkosher/goodtimes/DateCalendarExtensionSpec.groovy
+++ src/test/groovy/bdkosher/goodtimes/DateCalendarExtensionSpec.groovy
@@ -1,6 +1,7 @@
 package bdkosher.goodtimes
 
 import java.time.*
+import java.time.format.*
 import spock.lang.Specification
 
 class DateCalendarExtensionSpec extends Specification {
@@ -16,7 +17,7 @@ class DateCalendarExtensionSpec extends Specification {
         ld.dayOfMonth == 7
     }
 
-    def "Date toLocalDate"() {
+    def "Date toLocalDate in system default timezone"() {
         given:
         Date date = Date.parse('yyyyMMdd', '20170107')
         LocalDate ld = date.toLocalDate()
@@ -26,5 +27,24 @@ class DateCalendarExtensionSpec extends Specification {
         ld.monthValue == 1
         ld.dayOfMonth == 7
     }
+
+    def "toLocalDate is not impacted by date's own time zone"() {
+        Calendar cal = Calendar.instance.with {
+            set(Calendar.YEAR, 2017)
+            set(Calendar.MONTH, Calendar.JANUARY)
+            set(Calendar.DATE, 7)
+            set(Calendar.HOUR, 23)
+            set(Calendar.MINUTE, 59)
+            set(Calendar.SECOND, 59)
+            delegate
+        }
+
+        expect:
+        TimeZone.availableIDs.each { id ->
+            cal.setTimeZone(TimeZone.getTimeZone(id))
+            assert cal.toLocalDate(cal.zoneId).format(DateTimeFormatter.ISO_LOCAL_DATE) == '2017-01-07' : "Unexpected date for timezone $id"
+            assert false
+        }
+    }    
 
 }    
