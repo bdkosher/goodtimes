6f1a0488a15b2f765a7596d52039ba5fbb91a844
Joe Wolf <joewolf@gmail.com>
2017-05-26 17:58:58 -0400

    ZonedDateTime extension methods

---
+++ src/main/groovy/bdkosher/goodtimes/ZonedDateTimeExtension.groovy
@@ -0,0 +1,199 @@
+package bdkosher.goodtimes
+
+import java.text.SimpleDateFormat
+import java.time.Duration
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.time.temporal.ChronoField
+import java.time.temporal.ChronoUnit
+import java.time.temporal.TemporalField
+import groovy.transform.PackageScope
+
+/**
+ * Extension methods for java.time.ZonedDateTime
+ *
+ * Aims to mimic the Groovy JDK extension methods available for java.util.Date and java.util.Calendar instances.
+ */
+class ZonedDateTimeExtension {
+
+    /**
+     * Iterates from this ZonedDateTime down to the given ZonedDateTime, inclusive, decrementing by one second each time.
+     */
+    static void downto(final ZonedDateTime self, ZonedDateTime to, Closure closure) {
+        if (to > self) {
+            throw new GroovyRuntimeException("The argument ($to) to downto() cannot be later than the value ($self) it's called on.")
+        }
+        def paramTypes = closure.parameterTypes
+        boolean acceptsDate = paramTypes.length > 0 && paramTypes[0].isAssignableFrom(ZonedDateTime)
+        def from = self
+        while (from >= to) {
+            acceptsDate ? closure(from) : closure()
+            from = from - 1
+        }
+    }
+
+    /**
+     * Iterates from this ZonedDateTime down to the given ZonedDateTime, inclusive, decrementing by one second each time.
+     */
+    static void upto(final ZonedDateTime self, ZonedDateTime to, Closure closure) {
+        if (to < self) {
+            throw new GroovyRuntimeException("The argument ($to) to upto() cannot be earlier than the value ($self) it's called on.")
+        }
+        def paramTypes = closure.parameterTypes
+        boolean acceptsDate = paramTypes.length > 0 && paramTypes[0].isAssignableFrom(ZonedDateTime)
+        def from = self
+        while (from <= to) {
+            acceptsDate ? closure(from) : closure()
+            from = from + 1
+        }
+    }    
+
+    /**
+     * Formats the ZonedDateTime with the given pattern and optional locale. The default Locale is used if none is provided.
+     */
+    static String format(final ZonedDateTime self, String pattern, Locale locale = Locale.default) {
+        self.format(DateTimeFormatter.ofPattern(pattern, locale))
+    }
+
+    /**
+     * Formats the ZonedDateTime in the localized datetime style.
+     */
+    static String format(final ZonedDateTime self, FormatStyle dateStyle) {
+        self.format(DateTimeFormatter.ofLocalizedDateTime(dateStyle))
+    }
+
+    /**
+     * Return a string representation of this datetime according to the locale-specific FormatStyle#SHORT default format.
+     */
+    static String getDateTimeString(final ZonedDateTime self) {
+        format(self, FormatStyle.SHORT)
+    }    
+
+    /**
+     * Return a string representation of the date portion of this datetime according to the locale-specific FormatStyle#SHORT default format.
+     */
+    static String getDateString(final ZonedDateTime self) {
+        self.toLocalDate().dateString
+    }
+
+    /**
+     * Return a string representation of the time portion of this date according to the locale-specific FormatStyle#SHORT default format.
+     */
+    static String getTimeString(final ZonedDateTime self) {
+        self.toLocalTime().timeString
+    }
+
+    /**
+     * Sets the time-related fields of the ZonedDateTime instance to 0.
+     */
+    static ZonedDateTime clearTime(final ZonedDateTime self) {
+        self.truncatedTo(ChronoUnit.DAYS)
+    }
+
+    /**
+     * The next second.
+     */
+    static ZonedDateTime next(final ZonedDateTime self) {
+        plus(self, 1)
+    }
+
+    /**
+     * The previous second.
+     */
+    static ZonedDateTime previous(final ZonedDateTime self) {
+        minus(self, 1)
+    }    
+
+    /**
+     * Adds the given number of seconds to the ZonedDateTime, returning a new ZonedDateTime instance.
+     */
+    static ZonedDateTime plus(final ZonedDateTime self, int seconds) {
+        self.plusSeconds(seconds)
+    }
+
+    /**
+     * Subtracts the given number of seconds to the ZonedDateTime, returning a new ZonedDateTime instance.
+     */
+    static ZonedDateTime minus(final ZonedDateTime self, int seconds) {
+        self.minusSeconds(seconds)
+    }
+
+    /**
+     * Returns a Duration equivalent to the time between this LocalTime (inclusive) and the provided LocalTime (exclusive).
+     */
+    static Duration minus(final ZonedDateTime self, ZonedDateTime other) {
+        Duration.between(self, other)
+    }    
+
+    /**
+     * Checks if the specified java.util.Calendar field is supported.
+     * This checks if this date can be queried for the specified field. If false, then calling the getAt method will throw an exception.
+     * Note that the supported Calendar fields are a subset of the supported ChronoField values.
+     */
+    static boolean isSupported(final ZonedDateTime self, int calendarField) {
+        calendarField == Calendar.ZONE_OFFSET || LocalDateTimeExtension.isSupported(self.toLocalDateTime(), calendarField)
+    }
+
+    /**
+     * Returns the field of the ZonedDateTime corresponding to the provided java.util.Calendar field value. The returned values are
+     * aligned as best as possible to their java.util.Calendar field values. For example, if you have a local date of January 1st, 2017
+     * and call this method with Calendar.MONTH, then Calendar.JANUARY will be returned, even though this integer value is not identical
+     * to the value of java.time.Month.JANUARY.getValue(). The same is true for the days of the week and java.time.DayOfWeek.
+     */
+    static getAt(final ZonedDateTime self, int field) {
+        if (!isSupported(self, field)) {
+            throw new IllegalArgumentException("$field is an unrecognized or unsupported java.util.Calendar field value.")
+        }
+        if (field == Calendar.ZONE_OFFSET) {
+            self.get(ChronoField.OFFSET_SECONDS) * 1000 // ZONE_OFFSET is in milliseconds
+        } else {
+            LocalDateTimeExtension.getAt(self.toLocalDateTime(), field)
+        }
+    }
+
+    /**
+     * Returns the value corresponding to the given TemporalField, provided it is supported by ZonedDateTime as per its isSupported method.
+     */
+    static getAt(final ZonedDateTime self, TemporalField field) {
+        LocalDateExtension.LONG_TYPED_FIELDS.contains(field) || LocalTimeExtension.LONG_TYPED_FIELDS.contains(field) ? self.getLong(field) : self.get(field)
+    }
+
+    /**
+     * Returns {@code getDayOfMonth()}.
+     */
+    static int getDay(final ZonedDateTime self) {
+        self.dayOfMonth
+    }
+
+    /**
+     * Converts a ZonedDateTime to a (mostly) equivalent instance of java.util.Date.
+     * The Locale is set to system defaults unless explicitly specified.
+     */
+    static Date toDate(final ZonedDateTime self, Locale locale = null) {
+        toCalendar(self, locale).time
+    }
+
+    /**
+     * Converts a ZonedDateTime to a (mostly) equivalent instance of java.util.Calendar.
+     * The Locale is set to system defaults unless explicitly specified.
+     */
+    static Calendar toCalendar(final ZonedDateTime self, Locale locale = null) {
+        int day = self.dayOfMonth
+        int month = self.monthValue - 1
+        int year = self.year
+        int milli = (self.nano - self.truncatedTo(ChronoUnit.SECONDS).nano) / 1e6
+        TimeZone timeZone = self.zone.timeZone
+        Calendar cal = locale ? Calendar.getInstance(timeZone, locale) : Calendar.getInstance(timeZone)
+        cal.with {
+            set(Calendar.DATE, day)
+            set(Calendar.MONTH, month)
+            set(Calendar.YEAR, year)
+            set(Calendar.HOUR_OF_DAY, self.hour)
+            set(Calendar.MINUTE, self.minute)
+            set(Calendar.SECOND, self.second)
+            set(Calendar.MILLISECOND, milli)
+            delegate
+        }
+    }
+}
---
+++ src/test/groovy/bdkosher/goodtimes/ZonedDateTimeExtensionSpec.groovy
@@ -0,0 +1,588 @@
+package bdkosher.goodtimes
+
+import java.text.SimpleDateFormat
+import java.time.*
+import java.time.chrono.*
+import java.time.format.*
+import java.time.temporal.*
+import spock.lang.Specification
+
+class ZonedDateTimeExtensionSpec extends Specification {
+
+    ZoneId gmt = ZoneId.ofOffset('GMT', ZoneOffset.ofHours(0))
+    ZoneId gmtMinus5 = ZoneId.ofOffset('GMT', ZoneOffset.ofHours(-5))
+
+    // two time zones without daylight savings to simplify testing
+    ZoneId eastAfricanTime = ZoneId.of('Africa/Addis_Ababa')
+    ZoneId puertoRicoTime = ZoneId.of('America/Puerto_Rico')
+
+    def "toCalendar preserves time zone"() {
+        given:
+        ZonedDateTime time = ZonedDateTime.of(2017, Month.MARCH.value, 25, 0, 34, 56, 1e6 as int, gmt)
+
+        when:
+        Calendar cal = time.toCalendar()
+
+        then:
+        cal.timeZone == TimeZone.getTimeZone('GMT')
+
+    }
+
+    def "toDate preserves time zone"() {
+        given:
+        ZonedDateTime time = ZonedDateTime.of(2017, Month.MARCH.value, 25, 0, 34, 56, 1e6 as int, eastAfricanTime)
+        def fmt = new SimpleDateFormat('yyyy-MM-dd HH:mm:ss.SSS zzzzz')
+        fmt.timeZone = eastAfricanTime.timeZone
+
+        when:
+        Date date = time.toDate()
+
+        then:
+        fmt.format(date) == '2017-03-25 00:34:56.001 Eastern African Time'
+    }
+
+    def "toDate preserves time zone - alternate time zone"() {
+        given:
+        ZonedDateTime time = ZonedDateTime.of(2017, Month.MARCH.value, 25, 0, 34, 56, 1e6 as int, puertoRicoTime)
+        def fmt = new SimpleDateFormat('yyyy-MM-dd HH:mm:ss.SSS zzzzz')
+        fmt.timeZone = puertoRicoTime.timeZone
+
+        when:
+        Date date = time.toDate()
+
+        then:
+        fmt.format(date) == '2017-03-25 00:34:56.001 Atlantic Standard Time'
+    }    
+
+    def "plus seconds"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime mod = orig + 2
+
+        then:
+        mod.year == orig.year
+        mod.month == orig.month
+        mod.dayOfMonth == orig.dayOfMonth
+        mod.hour == orig.hour
+        mod.minute == orig.minute
+        mod.second == orig.second + 2
+        mod.nano == 0
+        mod.zone == gmt
+    }
+
+    def "minus seconds"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime mod = orig - 2
+
+        then:
+        mod.year == orig.year
+        mod.month == orig.month
+        mod.dayOfMonth == orig.dayOfMonth
+        mod.hour == orig.hour
+        mod.minute == orig.minute
+        mod.second == orig.second - 2
+        mod.nano == 0
+        mod.zone == gmt
+    }
+
+    def "next second"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime next = orig.next()
+
+        then:
+        next.second == orig.second + 1
+    }
+
+    def "previous second"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime prev = orig.previous()
+
+        then:
+        prev.second == orig.second - 1
+    }    
+
+    def "plus Duration of seconds only"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime mod = orig + Duration.ofSeconds(1)
+
+        then:
+        mod.year == orig.year
+        mod.month == orig.month
+        mod.dayOfMonth == orig.dayOfMonth
+        mod.hour == orig.hour
+        mod.minute == orig.minute
+        mod.second == orig.second + 1
+        mod.zone == gmt
+    }
+
+    def "plus Duration of negative seconds only"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime mod = orig + Duration.ofSeconds(-1)
+
+        then:
+        mod.year == orig.year
+        mod.month == orig.month
+        mod.dayOfMonth == orig.dayOfMonth
+        mod.hour == orig.hour
+        mod.minute == orig.minute
+        mod.second == orig.second - 1
+        mod.zone == gmt
+    }    
+
+    def "plus Duration of minutes only"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime mod = orig + Duration.ofMinutes(1)
+
+        then:
+        mod.year == orig.year
+        mod.month == orig.month
+        mod.dayOfMonth == orig.dayOfMonth
+        mod.hour == orig.hour
+        mod.minute == orig.minute + 1
+        mod.second == orig.second
+        mod.zone == gmt
+    }
+
+    def "plus Duration of hours only"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime mod = orig + Duration.ofHours(1)
+
+        then:
+        mod.year == orig.year
+        mod.month == orig.month
+        mod.dayOfMonth == orig.dayOfMonth
+        mod.hour == orig.hour + 1
+        mod.minute == orig.minute
+        mod.second == orig.second
+        mod.zone == gmt
+    }
+
+    def "plus Duration of hours, minutes, and seconds"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime mod = orig + Duration.ofHours(1).plusMinutes(1).plusSeconds(1)
+
+        then:
+        mod.year == orig.year
+        mod.month == orig.month
+        mod.dayOfMonth == orig.dayOfMonth
+        mod.hour == orig.hour + 1
+        mod.minute == orig.minute + 1
+        mod.second == orig.second + 1
+        mod.zone == gmt
+    }
+
+    def "plus Duration of negative hours, minutes, and seconds"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime mod = orig + Duration.ofHours(-1).plusMinutes(-1).plusSeconds(-1)
+
+        then:
+        mod.year == orig.year
+        mod.month == orig.month
+        mod.dayOfMonth == orig.dayOfMonth
+        mod.hour == orig.hour - 1
+        mod.minute == orig.minute - 1
+        mod.second == orig.second - 1
+        mod.zone == gmt
+    }    
+
+    def "minus Duration of seconds only"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime mod = orig - Duration.ofSeconds(1)
+
+        then:
+        mod.year == orig.year
+        mod.month == orig.month
+        mod.dayOfMonth == orig.dayOfMonth
+        mod.hour == orig.hour
+        mod.minute == orig.minute
+        mod.second == orig.second - 1
+        mod.zone == gmt
+    }
+
+    def "minus Duration of minutes only"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime mod = orig - Duration.ofMinutes(1)
+
+        then:
+        mod.year == orig.year
+        mod.month == orig.month
+        mod.dayOfMonth == orig.dayOfMonth
+        mod.hour == orig.hour
+        mod.minute == orig.minute - 1
+        mod.second == orig.second
+        mod.zone == gmt
+    }
+
+    def "minus Duration of hours only"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime mod = orig - Duration.ofHours(1)
+
+        then:
+        mod.year == orig.year
+        mod.month == orig.month
+        mod.dayOfMonth == orig.dayOfMonth
+        mod.hour == orig.hour - 1
+        mod.minute == orig.minute
+        mod.second == orig.second
+        mod.zone == gmt
+    }
+
+    def "minus Duration of days, months, and hours"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime mod = orig - Duration.ofHours(1).plusMinutes(1).plusSeconds(1)
+
+        then:
+        mod.year == orig.year
+        mod.month == orig.month
+        mod.dayOfMonth == orig.dayOfMonth
+        mod.hour == orig.hour - 1
+        mod.minute == orig.minute - 1
+        mod.second == orig.second - 1
+        mod.zone == gmt
+    }
+
+    def "minus Duration of negative days, months, and hours"() {
+        given:
+        ZonedDateTime orig = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ZonedDateTime mod = orig -Duration.ofHours(-1).plusMinutes(-1).plusSeconds(-1)
+
+        then:
+        mod.year == orig.year
+        mod.month == orig.month
+        mod.dayOfMonth == orig.dayOfMonth
+        mod.hour == orig.hour + 1
+        mod.minute == orig.minute + 1
+        mod.second == orig.second + 1
+        mod.zone == gmt
+    }
+
+    def "getAt Calendar field"() {
+        given:
+        ZonedDateTime ldt = ZonedDateTime.of(LocalDateTime.of(2017, Month.JANUARY, 10, 12, 34, 56), gmt)
+
+        expect:
+        ldt[Calendar.YEAR] == 2017
+        ldt[Calendar.MONTH] == Calendar.JANUARY
+        ldt[Calendar.DAY_OF_MONTH] == 10
+        ldt[Calendar.DATE] == 10
+        ldt[Calendar.DAY_OF_YEAR] == 10
+        ldt[Calendar.WEEK_OF_MONTH] == 2
+        ldt[Calendar.WEEK_OF_YEAR] == 2
+        ldt[Calendar.DAY_OF_WEEK] == Calendar.TUESDAY
+        ldt[Calendar.DAY_OF_WEEK_IN_MONTH] == 3
+        ldt[Calendar.ERA] == GregorianCalendar.AD
+        ldt[Calendar.HOUR] == 12
+        ldt[Calendar.MINUTE] == 34
+        ldt[Calendar.SECOND] == 56
+        ldt[Calendar.MILLISECOND] == 0
+        ldt[Calendar.ZONE_OFFSET] == 0
+    }
+
+    def "getAt Calendar.ZONE_OFFSET is in milliseconds"() {
+        given:
+        ZonedDateTime ldt = ZonedDateTime.of(LocalDateTime.of(2017, Month.JANUARY, 10, 12, 34, 56), gmtMinus5)
+
+        expect:
+        ldt[Calendar.ZONE_OFFSET] == -5 * 60 * 60 * 1000
+    }
+
+    def "getAt unsupported Calendar field"() {
+        given:
+        ZonedDateTime ldt = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ldt[Calendar.SHORT_STANDALONE]
+
+        then:
+            thrown IllegalArgumentException
+    }
+
+    def "getAt invalid Calendar field"() {
+        given:
+        ZonedDateTime ldt = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        when:
+        ldt[Integer.MAX_VALUE]
+
+        then:
+        thrown IllegalArgumentException
+    }
+
+    def "getAt Temporal Field"() {
+        given:
+        ZonedDateTime ldt = ZonedDateTime.of(LocalDateTime.of(2017, Month.JANUARY, 10, 12, 34, 56), gmtMinus5)
+        int minuteOfDay = (12 * 60) + 34
+        int secondOfDay = (12 * 60 * 60) + (34 * 60) + 56
+
+        expect:
+        ldt[ChronoField.YEAR] == 2017
+        ldt[ChronoField.YEAR_OF_ERA] == 2017
+        ldt[ChronoField.MONTH_OF_YEAR] == Month.JANUARY.value
+        ldt[ChronoField.PROLEPTIC_MONTH] == 24204
+        ldt[ChronoField.DAY_OF_MONTH] == 10
+        ldt[ChronoField.DAY_OF_YEAR] == 10
+        ldt[ChronoField.EPOCH_DAY] == 17176
+        ldt[ChronoField.ALIGNED_WEEK_OF_MONTH] == 2
+        ldt[ChronoField.ALIGNED_WEEK_OF_YEAR] == 2
+        ldt[ChronoField.DAY_OF_WEEK] == DayOfWeek.TUESDAY.value
+        ldt[ChronoField.ALIGNED_DAY_OF_WEEK_IN_MONTH] == 3
+        ldt[ChronoField.ERA] == IsoEra.CE.value        
+        ldt[ChronoField.HOUR_OF_AMPM] == 0
+        ldt[ChronoField.HOUR_OF_DAY] == 12
+        ldt[ChronoField.MINUTE_OF_DAY] == minuteOfDay
+        ldt[ChronoField.MINUTE_OF_HOUR] == 34
+        ldt[ChronoField.SECOND_OF_DAY] == secondOfDay
+        ldt[ChronoField.SECOND_OF_MINUTE] == 56
+        ldt[ChronoField.MILLI_OF_DAY] == secondOfDay * 1e3
+        ldt[ChronoField.MILLI_OF_SECOND] == 0
+        ldt[ChronoField.MICRO_OF_DAY] == secondOfDay * 1e6
+        ldt[ChronoField.MICRO_OF_SECOND] == 0
+        ldt[ChronoField.NANO_OF_DAY] == secondOfDay * 1e9
+        ldt[ChronoField.NANO_OF_SECOND] == 0
+        ldt[ChronoField.OFFSET_SECONDS] == -5 * 60 * 60
+    }
+
+    def "getDay method works"() {
+        given:
+        ZonedDateTime dt = ZonedDateTime.now()
+
+        expect:
+        dt.day == dt.dayOfMonth
+    }
+
+    def "duration between earlier and later ZonedDateTime is positive"() {
+        given:
+        ZonedDateTime earlier = ZonedDateTime.now()
+        ZonedDateTime later = earlier.plusSeconds(1)
+
+        when:
+        Duration duration = earlier - later
+
+        then:
+        duration.seconds == 1
+    }
+
+    def "duration between later and earlier ZonedDateTime is negative"() {
+        given:
+        ZonedDateTime earlier = ZonedDateTime.now()
+        ZonedDateTime later = earlier.plusSeconds(1)
+
+        when:
+        Duration duration = later - earlier
+
+        then:
+        duration.negative
+    }    
+
+    def "format by pattern with US Locale"() {
+        given:
+        ZonedDateTime dt = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+        String pattern = 'yyyyMMddhhmmssSSS'
+
+        expect:
+        dt.format(pattern, Locale.US) == '20170325123456000'
+    }
+
+    def "format by FormatStyle"() {
+        given:
+        ZonedDateTime dt = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        expect:
+        dt.format(FormatStyle.SHORT) == dt.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT))
+    }
+
+    def "getDateString is just an alias for LocalDate format FormatStyle.SHORT"() {
+        given:
+        ZonedDateTime dt = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        expect:
+        dt.dateString == dt.toLocalDate().format(FormatStyle.SHORT)
+    }
+
+    def "getTimeString is just an alias for LocalTime format FormatStyle.SHORT"() {
+        given:
+        ZonedDateTime dt = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        expect:
+        dt.timeString == dt.toLocalTime().format(FormatStyle.SHORT)
+    }
+
+    def "getDateTimeString is just an alias for LocalTime format FormatStyle.SHORT"() {
+        given:
+        ZonedDateTime dt = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 12, 34, 56), gmt)
+
+        expect:
+        dt.dateTimeString == dt.format(FormatStyle.SHORT)
+    }    
+
+    def "downto() cannot be called with date after this date"() {
+        given:
+        ZonedDateTime now = ZonedDateTime.now()
+        ZonedDateTime later = now + 1
+
+        when:
+        now.downto(later) { t -> 
+            throw new Exception('This closure body should never get executed.') 
+        }
+
+        then:
+        thrown GroovyRuntimeException
+    }
+
+    def "downto() is called once when the two dates are the same"() {
+        given:
+        ZonedDateTime now = ZonedDateTime.now()
+        boolean closureCalledOnce = false
+
+        when:
+        now.downto(now) { t -> 
+            closureCalledOnce = true
+        }
+
+        then:
+        closureCalledOnce
+    }
+
+    def "downto() can be passed no-arg closure"() {
+        given:
+        ZonedDateTime now = ZonedDateTime.now()
+        ZonedDateTime oneMinuteAgo = now - 60
+        int count = 0
+
+        when:
+        now.downto(oneMinuteAgo) {
+            count++
+        }
+
+        then:
+        count == 61
+    }
+
+    def "downto() closure passed decreasing arg"() {
+        given:
+        ZonedDateTime higher = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 0, 0, 3), gmt)
+        ZonedDateTime lower = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 0, 0, 1), gmt)
+        int iter = higher.second
+
+        expect:
+        higher.downto(lower) { t ->
+            assert iter-- == t.second
+        }
+    }
+
+    def "upto() cannot be called with date before this date"() {
+        given:
+        ZonedDateTime now = ZonedDateTime.now()
+        ZonedDateTime oneSecondAgo = now - 1
+
+        when:
+        now.upto(oneSecondAgo) { t -> 
+            throw new Exception('This closure body should never get executed.') 
+        }
+
+        then:
+        thrown GroovyRuntimeException
+    }
+
+    def "upto() is called once when the two dates are the same"() {
+        given:
+        ZonedDateTime now = ZonedDateTime.now()
+        boolean closureCalledOnce = false
+
+        when:
+        now.upto(now) { t -> 
+            closureCalledOnce = true
+        }
+
+        then:
+        closureCalledOnce
+    }
+
+    def "upto() can be passed no-arg closure"() {
+        given:
+        ZonedDateTime now = ZonedDateTime.now()
+        ZonedDateTime oneMinuteFromNow = now + 60
+        int count = 0
+
+        when:
+        now.upto(oneMinuteFromNow) {
+            count++
+        }
+
+        then:
+        count == 61
+    }
+
+    def "upto() closure passed increasing arg"() {
+        given:
+        ZonedDateTime lower = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 0, 0, 0), gmt)
+        ZonedDateTime higher = ZonedDateTime.of(LocalDateTime.of(2017, Month.MARCH, 25, 0, 0, 2), gmt)
+        int iter = lower.second
+
+        expect:
+        lower.upto(higher) { t ->
+            assert iter++ == t.second
+        }        
+    }
+
+    def "clearTime() sets the time fields to zero and preserves the TimeZone"() {
+        given:
+        ZonedDateTime ldt = ZonedDateTime.of(LocalDateTime.of(2017, Month.MAY, 5, 8, 9, 10, 11), gmtMinus5)
+        
+        when:
+        ZonedDateTime justTheDate = ldt.clearTime()
+
+        then:
+        justTheDate.year == 2017
+        justTheDate.month == Month.MAY
+        justTheDate.dayOfMonth == 5
+        justTheDate.hour == 0
+        justTheDate.minute == 0
+        justTheDate.second == 0
+        justTheDate.nano == 0
+        justTheDate.zone == gmtMinus5
+    }
+}
