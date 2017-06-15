061c0ff1d4d0c78da823a0ea6838e83f89252c38
Joe Wolf <joewolf@gmail.com>
2017-06-14 22:52:28 -0400

    OffsetTime extension methods

---
+++ src/main/groovy/bdkosher/goodtimes/OffsetTimeExtension.groovy
@@ -0,0 +1,160 @@
+package bdkosher.goodtimes
+
+import java.time.*
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.time.temporal.*
+
+import groovy.transform.PackageScope
+
+/**
+ * Extension methods for java.time.OffsetTime
+ *
+ * Aims to mimic the Groovy JDK extension methods available for java.util.Date and java.util.Calendar instances.
+ */
+class OffsetTimeExtension {
+    
+    /**
+     * Iterates from this OffsetTime down to the given OffsetTime, inclusive, decrementing by one second each time.
+     */
+    static void downto(final OffsetTime self, OffsetTime to, Closure closure) {
+        if (to > self) {
+            throw new GroovyRuntimeException("The argument ($to) to downto() cannot be later than the value ($self) it's called on.")
+        }
+        def paramTypes = closure.parameterTypes
+        boolean acceptsDate = paramTypes.length > 0 && paramTypes[0].isAssignableFrom(OffsetTime)
+        def from = self
+        while (from >= to) {
+            acceptsDate ? closure(from) : closure()
+            from = from - 1
+        }
+    }
+
+    /**
+     * Iterates from this OffsetTime down to the given OffsetTime, inclusive, decrementing by one second each time.
+     */
+    static void upto(final OffsetTime self, OffsetTime to, Closure closure) {
+        if (to < self) {
+            throw new GroovyRuntimeException("The argument ($to) to upto() cannot be earlier than the value ($self) it's called on.")
+        }
+        def paramTypes = closure.parameterTypes
+        boolean acceptsDate = paramTypes.length > 0 && paramTypes[0].isAssignableFrom(OffsetTime)
+        def from = self
+        while (from <= to) {
+            acceptsDate ? closure(from) : closure()
+            from = from + 1
+        }
+    }    
+
+    /**
+     * Formats the OffsetTime with the given pattern and optional locale. The default Locale is used if none is provided.
+     */
+    static String format(final OffsetTime self, String pattern, Locale locale = Locale.default) {
+        self.format(DateTimeFormatter.ofPattern(pattern, locale))
+    }
+
+    /**
+     * Formats the OffsetTime in the localized time style.
+     */
+    static String format(final OffsetTime self, FormatStyle timeStyle) {
+        self.format(DateTimeFormatter.ofLocalizedTime(timeStyle))
+    }
+
+    /**
+     * Return a string representation of this time according to the locale-specific FormatStyle#SHORT default format.
+     */
+    static String getTimeString(final OffsetTime self) {
+        format(self, FormatStyle.SHORT)
+    }
+
+    /**
+     * The next second.
+     */
+    static OffsetTime next(final OffsetTime self) {
+        plus(self, 1)
+    }
+
+    /**
+     * The previous second.
+     */
+    static OffsetTime previous(final OffsetTime self) {
+        minus(self, 1)
+    }    
+
+    /**
+     * Adds the given number of seconds to the OffsetTime, returning a new OffsetTime instance.
+     */
+    static OffsetTime plus(final OffsetTime self, int seconds) {
+        self.plusSeconds(seconds)
+    }
+
+    /**
+     * Subtracts the given number of seconds to the OffsetTime, returning a new OffsetTime instance.
+     */
+    static OffsetTime minus(final OffsetTime self, int seconds) {
+        self.minusSeconds(seconds)
+    }
+
+    /**
+     * Returns a TemporalAmount equivalent to the time between this OffsetTime (inclusive) and the provided OffsetTime (exclusive).
+     */
+    static Duration minus(final OffsetTime self, OffsetTime other) {
+        Duration.between(self, other)
+    }
+
+    /**
+     * Checks if the specified java.util.Calendar field is supported.
+     * This checks if this date can be queried for the specified field. If false, then calling the getAt method will throw an exception.
+     * Note that the supported Calendar fields are a subset of the supported ChronoField values.
+     */
+    static boolean isSupported(final OffsetTime self, int calendarField) {
+        calendarField == Calendar.ZONE_OFFSET || LocalTimeExtension.isSupported(self.toLocalTime(), calendarField)
+    }
+
+    /**
+     * Returns the field of the OffsetTime corresponding to the provided java.util.Calendar field value.
+     */
+    static getAt(final OffsetTime self, int field) {
+        if (!isSupported(self, field)) {
+            throw new IllegalArgumentException("$field is an unrecognized or unsupported java.util.Calendar field value.")
+        }
+        if (field == Calendar.ZONE_OFFSET) {
+            self.get(ChronoField.OFFSET_SECONDS) * 1000 // ZONE_OFFSET is in milliseconds
+        } else {
+            LocalTimeExtension.getAt(self.toLocalTime(), field)
+        }
+    }
+
+    /**
+     * Returns the value corresponding to the given TemporalField, provided it is supported by OffsetTime as per its isSupported method.
+     */
+    static getAt(final OffsetTime self, TemporalField field) {
+        LocalTimeExtension.LONG_TYPED_FIELDS.contains(field) ? self.getLong(field) : self.get(field)
+    }
+
+    /**
+     * Returns a LocalDateTime of this OffsetTime and the given LocalDate.
+     */
+    static OffsetDateTime leftShift(final OffsetTime self, LocalDate date) {
+        OffsetDateTime.of(date, self.toLocalTime(), self.offset)
+    }    
+
+    /**
+     * Converts a OffsetTime to a (mostly) equivalent instance of java.util.Date. The day-month-year value of the returned Date is now,
+     * and the TimeZone and Locale are set to system defaults unless explicitly specified. Time is truncated to nearest millisecond.
+     */
+    static Date toDate(final OffsetTime self, Locale locale = null) {
+        toCalendar(self, locale).time
+    }
+
+    /**
+     * Converts a OffsetTime to a (mostly) equivalent instance of java.util.Calendar. The time value of the returned Calendar is cleared,
+     * and the TimeZone and Locale are set to system defaults unless explicitly specified. Time is truncated to nearest millisecond.
+     */
+    static Calendar toCalendar(final OffsetTime self, Locale locale = null) {
+        int offsetMilli = self.offset.totalSeconds * 1000
+        Calendar cal = self.toLocalTime().toCalendar((Locale) locale)
+        cal.set(Calendar.ZONE_OFFSET, offsetMilli)
+        cal
+    }
+}
---
+++ src/test/groovy/bdkosher/goodtimes/OffsetTimeExtensionSpec.groovy
@@ -0,0 +1,341 @@
+package bdkosher.goodtimes
+
+import java.time.*
+import java.time.chrono.*
+import java.time.format.*
+import java.time.temporal.*
+import spock.lang.Specification
+
+class OffsetTimeExtensionSpec extends Specification {
+
+    ZoneOffset minusFive = ZoneOffset.ofHours(-5)
+    ZoneOffset hmsOffset = ZoneOffset.ofHoursMinutesSeconds(1, 1, 1)
+
+    def "toCalendar preserves offset"() {
+        given:
+        OffsetTime time = OffsetTime.of(LocalTime.of(12, 34, 56), minusFive)
+
+        when:
+        Calendar cal = time.toCalendar()
+
+        then:
+        cal.get(Calendar.HOUR_OF_DAY) == 12
+        cal.get(Calendar.MINUTE) == 34
+        cal.get(Calendar.SECOND) == 56
+        cal.get(Calendar.MILLISECOND) == 0
+        cal.get(Calendar.ZONE_OFFSET) == -5 * 60 * 60 * 1000
+    }
+
+    def "toCalendar preserves offset - alternate offset"() {
+        given:
+        OffsetTime time = OffsetTime.of(LocalTime.of(12, 34, 56, 78e6 as int), hmsOffset)
+
+        when:
+        Calendar cal = time.toCalendar()
+
+        then:
+        cal.get(Calendar.HOUR_OF_DAY) == 12
+        cal.get(Calendar.MINUTE) == 34
+        cal.get(Calendar.SECOND) == 56
+        cal.get(Calendar.MILLISECOND) == 78
+        cal.get(Calendar.ZONE_OFFSET) == (1 * 60 * 60 * 1000) + (1 * 60 * 1000) + (1 * 1000)
+    }
+
+    def "toDate works decently enough as you could expect"() {
+        given:
+        OffsetTime time = OffsetTime.of(LocalTime.of(12, 34, 56), minusFive)
+
+        when:
+        Date date = time.toDate()
+
+        then:
+        date.format('HH:mm:ss.SSS') == '12:34:56.000'
+    }
+
+    def "plus seconds"() {
+        given:
+        OffsetTime orig = OffsetTime.of(12, 34, 56, 78, minusFive)
+
+        when:
+        OffsetTime mod = orig + 2
+
+        then:
+        mod.hour == orig.hour
+        mod.minute == orig.minute
+        mod.second == orig.second + 2
+        mod.nano == orig.nano
+        mod.offset == orig.offset
+    }
+
+    def "minus seconds"() {
+        given:
+        OffsetTime orig = OffsetTime.of(12, 34, 56, 78, minusFive)
+
+        when:
+        OffsetTime mod = orig - 2
+
+        then:
+        mod.hour == orig.hour
+        mod.minute == orig.minute
+        mod.second == orig.second - 2
+        mod.nano == orig.nano
+        mod.offset == orig.offset
+    }
+
+    def "next second"() {
+        given:
+        OffsetTime orig = OffsetTime.of(12, 34, 56, 78, minusFive)
+
+        when:
+        OffsetTime next = orig.next()
+
+        then:
+        next.second == orig.second + 1
+    }
+
+    def "previous second"() {
+        given:
+        OffsetTime orig = OffsetTime.of(12, 34, 56, 78, minusFive)
+
+        when:
+        OffsetTime prev = orig.previous()
+
+        then:
+        prev.second == orig.second - 1
+    }    
+
+    def "getAt Calendar field"() {
+        given:
+        OffsetTime lt = OffsetTime.of(12, 34, 56, 78, minusFive)
+
+        expect:
+        lt[Calendar.HOUR] == 12
+        lt[Calendar.MINUTE] == 34
+        lt[Calendar.SECOND] == 56
+        lt[Calendar.MILLISECOND] == 0
+        lt[Calendar.ZONE_OFFSET] == -5 * 60 * 60 * 1000
+    }
+
+    def "getAt unsupported Calendar field"() {
+        given:
+        OffsetTime lt = OffsetTime.of(12, 34, 56, 78, minusFive)
+
+        when:
+        lt[Calendar.ERA]
+
+        then:
+        thrown IllegalArgumentException
+    }
+
+    def "getAt invalid Calendar field"() {
+        given:
+        OffsetTime lt = OffsetTime.of(12, 34, 56, 78, minusFive)
+
+        when:
+        lt[Integer.MAX_VALUE]
+
+        then:
+        thrown IllegalArgumentException
+    }
+
+    def "getAt Temporal Field"() {
+        given:
+        OffsetTime lt = OffsetTime.of(12, 34, 56, 0, minusFive)
+        int minuteOfDay = (12 * 60) + 34
+        int secondOfDay = (12 * 60 * 60) + (34 * 60) + 56
+        int offsetSeconds = -5 * 60 * 60
+
+        expect:
+        lt[ChronoField.HOUR_OF_AMPM] == 0
+        lt[ChronoField.HOUR_OF_DAY] == 12
+        lt[ChronoField.MINUTE_OF_DAY] == minuteOfDay
+        lt[ChronoField.MINUTE_OF_HOUR] == 34
+        lt[ChronoField.SECOND_OF_DAY] == secondOfDay
+        lt[ChronoField.SECOND_OF_MINUTE] == 56
+        lt[ChronoField.MILLI_OF_DAY] == secondOfDay * 1e3
+        lt[ChronoField.MILLI_OF_SECOND] == 0
+        lt[ChronoField.MICRO_OF_DAY] == secondOfDay * 1e6
+        lt[ChronoField.MICRO_OF_SECOND] == 0
+        lt[ChronoField.NANO_OF_DAY] == secondOfDay * 1e9
+        lt[ChronoField.NANO_OF_SECOND] == 0
+        lt[ChronoField.OFFSET_SECONDS] == offsetSeconds
+    }
+
+    def "duration between T and an-hour-from-T is 1 hour when T is more than an hour from midnight"() {
+        given:
+        OffsetTime t = OffsetTime.of(0, 0, 0, 0, minusFive)
+        OffsetTime hourFromT = t.plusHours(1)
+
+        when:
+        Duration duration = t - hourFromT
+
+        then:
+        duration.seconds == 60 * 60
+    }
+
+    def "duration between an-hour-from-T and T is -1 hour when T is more than an hour from midnight"() {
+        given:
+        OffsetTime t = OffsetTime.of(0, 0, 0, 0, minusFive)
+        OffsetTime hourFromT = t.plusHours(1)
+
+        when:
+        Duration duration = hourFromT - t
+
+        then:
+        duration.seconds == -1 * 60 * 60
+    }
+
+    def "format by pattern with US Locale"() {
+        given:
+        OffsetTime t = OffsetTime.of(12, 34, 56, 0, minusFive)
+        String pattern = 'hhmmssSSS'
+
+        expect:
+        t.format(pattern, Locale.US) == '123456000'
+    }
+
+    def "format by FormatStyle"() {
+        given:
+        OffsetTime t = OffsetTime.of(12, 34, 56, 78, minusFive)
+
+        expect:
+        t.format(FormatStyle.SHORT) == t.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT))
+    }
+
+    def "getTimeString is just an alias for format FormatStyle.SHORT"() {
+        given:
+        OffsetTime t = OffsetTime.of(12, 34, 56, 78, minusFive)
+
+        expect:
+        t.timeString == t.format(FormatStyle.SHORT)
+    }
+
+    def "downto() cannot be called with date after this date"() {
+        setup:
+        OffsetTime now = OffsetTime.now()
+        OffsetTime later = now + 1
+
+        when:
+        now.downto(later) { t -> 
+            throw new Exception('This closure body should never get executed.') 
+        }
+
+        then:
+        thrown GroovyRuntimeException
+    }
+
+    def "downto() is called once when the two dates are the same"() {
+        setup:
+        OffsetTime now = OffsetTime.now()
+        boolean closureCalledOnce = false
+
+        when:
+        now.downto(now) { t -> 
+            closureCalledOnce = true
+        }
+
+        then:
+        closureCalledOnce
+    }
+
+    def "downto() can be passed no-arg closure"() {
+        setup:
+        OffsetTime now = OffsetTime.now()
+        OffsetTime oneMinuteAgo = now - 60
+        int count = 0
+
+        when:
+        now.downto(oneMinuteAgo) {
+            count++
+        }
+
+        then:
+        count == 61
+    }
+
+    def "downto() closure passed decreasing arg"() {
+        setup:
+        OffsetTime higher = OffsetTime.of(0, 0, 3, 0, minusFive)
+        OffsetTime lower = OffsetTime.of(0, 0, 1, 0, minusFive)
+        int iter = higher.second
+
+        expect:
+        higher.downto(lower) { t ->
+            assert iter-- == t.second
+        }
+    }
+
+    def "upto() cannot be called with date before this date"() {
+        setup:
+        OffsetTime now = OffsetTime.now()
+        OffsetTime oneSecondAgo = now - 1
+
+        when:
+        now.upto(oneSecondAgo) { t -> 
+            throw new Exception('This closure body should never get executed.') 
+        }
+
+        then:
+        thrown GroovyRuntimeException
+    }
+
+    def "upto() is called once when the two dates are the same"() {
+        setup:
+        OffsetTime now = OffsetTime.now()
+        boolean closureCalledOnce = false
+
+        when:
+        now.upto(now) { t -> 
+            closureCalledOnce = true
+        }
+
+        then:
+        closureCalledOnce
+    }
+
+    def "upto() can be passed no-arg closure"() {
+        given:
+        OffsetTime now = OffsetTime.now()
+        OffsetTime oneMinuteFromNow = now + 60
+        int count = 0
+
+        when:
+        now.upto(oneMinuteFromNow) {
+            count++
+        }
+
+        then:
+        count == 61
+    }
+
+    def "upto() closure passed increasing arg"() {
+        given:
+        OffsetTime lower = OffsetTime.of(0, 0, 0, 0, minusFive)
+        OffsetTime higher = OffsetTime.of(0, 0, 2, 0, minusFive)
+        int iter = lower.second
+
+        expect:
+        lower.upto(higher) { t ->
+            assert iter++ == t.second
+        }        
+    }
+
+    def "leftShifting a LocalDate produces a OffsetDateTime"() {
+        given:
+        OffsetTime time = OffsetTime.of(12, 34, 56, 78, minusFive)
+        LocalDate date = LocalDate.of(2017, Month.JULY, 4)
+        
+        when:
+        OffsetDateTime datetime = time << date
+
+        then:
+        datetime.year == date.year
+        datetime.month == date.month
+        datetime.dayOfMonth == date.day
+        datetime.hour == time.hour
+        datetime.minute == time.minute
+        datetime.second == time.second
+        datetime.nano == time.nano
+        datetime.offset == time.offset
+    }
+}
