489053b7a8f8a54a31f2faf9867a076451c0ecec
Joe Wolf <joewolf@gmail.com>
2017-07-05 20:56:21 -0400

    fixing offset-adjustment issue in toCalendar()

--- src/main/groovy/bdkosher/goodtimes/OffsetTimeExtension.groovy
+++ src/main/groovy/bdkosher/goodtimes/OffsetTimeExtension.groovy
@@ -1,9 +1,14 @@
 package bdkosher.goodtimes
 
-import java.time.*
+import java.time.Duration
+import java.time.LocalDate
+import java.time.OffsetDateTime
+import java.time.OffsetTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
-import java.time.temporal.*
+import java.time.temporal.ChronoField
+import java.time.temporal.ChronoUnit
+import java.time.temporal.TemporalField
 
 import groovy.transform.PackageScope
 
